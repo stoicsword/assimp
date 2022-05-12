ebcac04bea28161c6f65af6ea2911b95083956d2
Xie Hao Zhe <xiehaozhe@gmail.com>
2022-05-12 15:50:12 +0800

    commit initial files

---
+++ BINARIES/Win32/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/khoow/Documents/GAM300-350/GAM300-350/andromeda/dependencies/assimp/contrib/zlib
+# Build directory: C:/Users/khoow/Documents/GAM300-350/GAM300-350/andromeda/dependencies/assimp/BINARIES/Win32/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
